325ff5f39ecd40d0c948cccb82770603275b916b
Shahid Afridi <siddiquibabuddin202@gmail.com>
2023-07-29 22:27:55 +0530

    Comment added

--- app.py
+++ app.py
@@ -310,6 +310,153 @@ if __name__ == '__main__':
 def create_app():
     return app    
 
+# # Define the Snack model to store snack information
+# class Snack(db.Model):
+#     # Columns for the Snack table
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     price = db.Column(db.Float, nullable=False)
+#     availability = db.Column(db.String(50), default='0')
+#     image_url = db.Column(db.String(200), nullable=False, default='https://via.placeholder.com/150')
+
+#     def __repr__(self):
+#         return f"Snack(id={self.id}, name='{self.name}', price={self.price}, availability='{self.availability}', image_url='{self.image_url}')"
+
+# # Define the Order model to store order information
+# class Order(db.Model):
+#     __tablename__ = 'orders'
+#     # Columns for the Order table
+#     id = db.Column(db.Integer, primary_key=True)
+#     customer_name = db.Column(db.String(255), nullable=False)
+#     item_id = db.Column(db.Integer, nullable=False)
+#     item_name = db.Column(db.String(255), nullable=False)
+#     status = db.Column(db.String(255))
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+#     def __init__(self, customer_name, item_id, item_name, status, user_id):
+#         self.customer_name = customer_name
+#         self.item_id = item_id
+#         self.item_name = item_name
+#         self.status = status
+#         self.user_id = user_id
+
+#     def __repr__(self):
+#         return f"Order(id={self.id}, customer_name='{self.customer_name}', item_id={self.item_id}, item_name='{self.item_name}', status='{self.status}', user_id={self.user_id})"
+
+# # Define the User model to store user information
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(255), unique=True, nullable=False)
+#     password = db.Column(db.String(255), nullable=False)
+#     email = db.Column(db.String(255), unique=True, nullable=False)
+#     orders = db.relationship('Order', backref='user', lazy=True)
+
+#     def __repr__(self):
+#         return f"User(id={self.id}, username='{self.username}', email='{self.email}')"
+
+# # Create the database tables (Snack, Order, User)
+# with app.app_context():
+#     db.create_all()
+
+# # The following routes handle the basic functionalities of the application:
+
+# @app.route("/")
+# def hello():
+#     return "Welcome to Zagato Zomato"
+
+# @app.route('/favicon.ico')
+# def favicon():
+#     return '', 204
+
+# # User registration and login routes
+
+# @app.route('/register', methods=['POST']) 
+# def register():
+#     # Register a new user with the provided username, password, and email
+#     # Check if the username or email already exists in the users table
+#     # Create a new user and add it to the database
+#     # Return a message indicating successful registration or an error message if the username or email already exists.
+
+# @app.route('/login', methods=['POST'])
+# def login():
+#     # Log in an existing user with the provided username/email and password
+#     # Check if the username/email exists in the users table
+#     # Check if the password matches the user's password in the database
+#     # Return a success message if login is successful or an error message if the user does not exist or the password is incorrect.
+
+# @app.route('/getuserId/<username>/<password>', methods=['GET'])
+# def getUserId(username, password):
+#     # Retrieve the user ID of a user with the provided username and password
+#     # Return the user ID if the user exists, or an error message if the username and password combination is invalid.
+
+# # Snack-related routes
+
+# @app.route('/addsnack', methods=['POST'])
+# def addSnack():
+#     # Add a new snack to the database with the provided snack details (name, price, availability, image_url)
+#     # Return the details of the added snack, including the assigned ID.
+
+# @app.route('/viewsnack', methods=['GET'])
+# def viewSnack():
+#     # Retrieve and return details of all snacks available in the database.
+
+# @app.route('/viewtopsnacks', methods=['GET'])
+# def viewTopSnacks():
+#     # Retrieve and return details of the top 5 snacks with the highest price.
+
+# @app.route('/updatesnack/<int:id>', methods=['PUT'])
+# def updateSnack(id):
+#     # Update the details of a snack with the provided ID in the database.
+#     # Return the updated details of the snack.
+
+# @app.route('/deletesnack/<int:id>', methods=['DELETE'])
+# def deleteSnack(id):
+#     # Delete the snack with the provided ID from the database.
+#     # Return a message indicating successful deletion or an error message if the snack with the given ID does not exist.
+
+# # Order-related routes
+
+# @app.route('/takeorder/<int:user_id>', methods=['POST'])
+# def takeOrder(user_id):
+#     # Create a new order for a user with the provided details (customer name, item ID)
+#     # Check if the snack with the given item ID exists in the database
+#     # Add the new order to the database
+#     # Return a message indicating successful order placement or an error message if the snack with the given item ID does not exist.
+
+# @app.route('/viewtoporders', methods=['GET'])
+# def viewTopOrders():
+#     # Retrieve and return details of the top 5 orders with the highest IDs.
+
+# @app.route('/viewallorder', methods=['GET'])
+# def viewallOrder():
+#     # Retrieve and return details of all orders in the database.
+
+# @app.route('/updateorder/<int:order_id>', methods=['PUT'])
+# def updateOrder(order_id):
+#     # Update the status of an order with the provided ID in the database.
+#     # Return a message indicating successful status update or an error message if the order with the given ID does not exist.
+
+# @app.route('/vieworders/<int:user_id>', methods=['GET'])
+# def view_orders(user_id):
+#     # Retrieve and return details of all orders associated with a user with the provided user ID.
+
+# @app.route('/updateOrderStatus/<int:id>', methods=['PUT'])
+# def updateOrderStatus(id):
+#     # Update the status of an order with the provided ID in the database.
+#     # Return the updated status of the order.
+
+# @app.route('/deleteorder/<int:user_id>/<int:order_id>', methods=['DELETE'])
+# def deleteOrder(user_id, order_id):
+#     # Delete an order with the provided order ID associated with a user with the provided user ID.
+#     # Return a message indicating successful deletion or an error message if the order with the given user ID and order ID does not exist.
+
+# if __name__ == '__main__':
+#     app.run(port=8080)
+
+# def create_app():
+#     return app
+
+
 #  Link of this project 
 
 #  Netlify Link :-  https://spontaneous-malasada-746986.netlify.app/
